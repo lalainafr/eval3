e2df3457be736ad8d5772f3104f5d5dec35f319f
Lalaina Rajaonahsoa <r.lalainafr@gmail.com>
2021-03-15 17:05:36 +0100

    Cibling HTML elements

---
+++ script.js
@@ -0,0 +1,13 @@
+//Cibler les elements HTML
+let newGameBtn = document.getElementById('newGame')
+let tour1 =  document.getElementById('tour1')
+let tour2 =  document.getElementById('tour2')
+let textDisplay =  document.getElementById('textDisplay')
+let p1GlobalScore = document.getElementById('p1GlobalScore')
+let p2GlobalScore = document.getElementById('p2GlobalScore')
+let dice = document.getElementById('dice')
+let p1RoundScore = document.getElementById('p1RoundScore')
+let p2RoundScore = document.getElementById('p2RoundScore')
+let rollDiceBtn = document.getElementById('rollDice')
+let holdBtn = document.getElementById('hold')
+
